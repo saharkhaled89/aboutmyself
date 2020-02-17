97365ad4f04e84c0d514c77ff27690b0cf83ddd2
sahar khaled <engineer.sahar.8989@hotmail.com>
2020-02-17 16:24:22 +0200

    add

---
+++ js/app.js
@@ -0,0 +1,88 @@
+
+'use strict';
+
+
+var name=prompt('whats your name?');
+console.log(" user name = "  + name);
+alert (" welcome to my page"   + name);
+
+
+
+    var study=prompt('Am I a chemical engineer?').toLocaleLowerCase();
+if (study==="yes" || study==="y") {
+        console.log("yes thats correct");
+    alert("yes thats correct" + study);
+
+
+} else if (study==="no"|| study==="n"){
+        console.log("no thats not correct");
+        alert("no thats not correct" + study);
+    }
+    
+
+
+    var myName=prompt('my names sahar?').toLocaleLowerCase();
+if (myName==="yes" || myName==="y") {
+        console.log("yes thats correct welcome to my page");
+        alert("yes thats correct welcome to my page" + myName );
+
+    } else if (myName==="no"|| myName==="n"){
+        console.log("no thats not correct");
+        alert("no thats not correct" + myName);
+    }
+
+    var work=prompt('My WORK EXPERIENCE Facilitator of mathematics and science ?').toLocaleLowerCase();
+if (work==="yes" || work==="y") {
+        console.log("yes thats correct my work EXPERIENCE Facilitator of mathematics and science");
+    alert(" yes thats correct my work EXPERIENCE Facilitator of mathematics and science " + work);
+} else if (work==="no"|| work==="n"){
+        console.log("no thats not correct");
+        alert("no thats not correct" + work);
+    }
+
+
+    var project=prompt('is my project Generate bio-gas and fertilizers from organic waste ?').toLocaleLowerCase();
+if (project==="yes" || project==="y") {
+        console.log("yes thats correct my project Generate bio-gas and fertilizers from organic waste  ");
+    alert(" yes thats correct my project Generate bio-gas and fertilizers from organic waste  " + project);
+} else if (project==="no"|| project==="n"){
+        console.log("no thats not correct");
+        alert("no thats not correct" + project);
+    }
+
+
+
+    var graduate=prompt('Is the graduation project name The effect of temperature and PH on settlability ?').toLocaleLowerCase();
+if (graduate==="yes" || graduate==="y") {
+        console.log("yes thats correct my graduation project name The effect of temperature and PH on settlability");
+    alert(" yes thats correct graduation project name The effect of temperature and PH on settlability " + graduate);
+} else if (project==="no"|| project==="n"){
+        console.log("no thats not correct");
+        alert("no thats not correct" + graduate);
+    }
+
+    var name=prompt('whats your name?');
+    console.log(" user name = "  + name);
+    alert (" welcome to my page"   + name);
+    document.write("welcome to my page   "   + name);
+    
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+  
+    
