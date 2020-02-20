ac4d3fd78277d780e5a6cbf7fc1e5fbd4e0616fb
sahar khaled <engineer.sahar.8989@hotmail.com>
2020-02-20 08:42:13 +0200

    add

--- js/app.js
+++ js/app.js
@@ -6,31 +6,31 @@ alert(' welcome to my page' + isName);
 
 
 function myQuestion1(){
-var study = prompt('Am I a chemical engineer?').toLocaleLowerCase();
-if (study === 'yes' || study === 'y') {
-  console.log('yes thats correct');
-  alert('yes thats correct' + study);
-  Degree=Degree+1;
-} else if (study === 'no' || study === 'n') {
-  console.log('no thats not correct');
-  alert('no thats not correct' + study);
-}
+  var study = prompt('Am I a chemical engineer?').toLocaleLowerCase();
+  if (study === 'yes' || study === 'y') {
+    console.log('yes thats correct');
+    alert('yes thats correct' + study);
+    Degree=Degree+1;
+  } else if (study === 'no' || study === 'n') {
+    console.log('no thats not correct');
+    alert('no thats not correct' + study);
+  }
 
 }
 myQuestion1();
 
 function myQuestion2(){
 
-var myName = prompt('my names sahar?').toLocaleLowerCase();
-if (myName === 'yes' || myName === 'y') {
-  console.log('yes thats correct welcome to my page');
-  alert('yes thats correct welcome to my page" + myName');
-  Degree=Degree+1;
+  var myName = prompt('my names sahar?').toLocaleLowerCase();
+  if (myName === 'yes' || myName === 'y') {
+    console.log('yes thats correct welcome to my page');
+    alert('yes thats correct welcome to my page" + myName');
+    Degree=Degree+1;
 
-} else if (myName === 'no' || myName === 'n') {
-  console.log('no thats not correct');
-  alert('no thats not correct' + myName);
-}
+  } else if (myName === 'no' || myName === 'n') {
+    console.log('no thats not correct');
+    alert('no thats not correct' + myName);
+  }
 
 }
 
@@ -39,49 +39,49 @@ myQuestion2();
 
 function myQuestion3(){
 
-var work = prompt('My WORK EXPERIENCE Facilitator of mathematics and science ?').toLocaleLowerCase();
-if (work === 'yes' || work === 'y') {
-  console.log('yes thats correct my work EXPERIENCE Facilitator of mathematics and science');
-  alert('yes thats correct my work EXPERIENCE Facilitator of mathematics and science ' + work);
-  Degree=Degree+1;
-} else if (work === 'no' || work === 'n') {
-  console.log('no thats not correct');
-  alert('no thats not correct' + work);
-}
+  var work = prompt('My WORK EXPERIENCE Facilitator of mathematics and science ?').toLocaleLowerCase();
+  if (work === 'yes' || work === 'y') {
+    console.log('yes thats correct my work EXPERIENCE Facilitator of mathematics and science');
+    alert('yes thats correct my work EXPERIENCE Facilitator of mathematics and science ' + work);
+    Degree=Degree+1;
+  } else if (work === 'no' || work === 'n') {
+    console.log('no thats not correct');
+    alert('no thats not correct' + work);
+  }
 
 
 }
- myQuestion3();
+myQuestion3();
 
 
- function myQuestion4(){
+function myQuestion4(){
 
-var project = prompt('is my project Generate bio-gas and fertilizers from organic waste ?').toLocaleLowerCase();
-if (project === 'yes' || project === 'y') {
-  console.log('yes thats correct my project Generate bio-gas and fertilizers from organic waste  ');
-  alert(' yes thats correct my project Generate bio-gas and fertilizers from organic waste ' + project);
-  Degree=Degree+1;
-} else if (project === 'no' || project === 'n') {
-  console.log('no thats not correct');
-  alert('no thats not correct' + project);
+  var project = prompt('is my project Generate bio-gas and fertilizers from organic waste ?').toLocaleLowerCase();
+  if (project === 'yes' || project === 'y') {
+    console.log('yes thats correct my project Generate bio-gas and fertilizers from organic waste  ');
+    alert(' yes thats correct my project Generate bio-gas and fertilizers from organic waste ' + project);
+    Degree=Degree+1;
+  } else if (project === 'no' || project === 'n') {
+    console.log('no thats not correct');
+    alert('no thats not correct' + project);
+  }
+
 }
-
- }
- myQuestion4();
+myQuestion4();
 
 
 function myQuestion5(){
 
 
-var graduate = prompt('Is the graduation project name The effect of temperature and PH on settlability ?').toLocaleLowerCase();
-if (graduate === 'yes' || graduate === 'y') {
-  console.log('yes thats correct my graduation project name The effect of temperature and PH on settlability');
-  alert( 'yes thats correct graduation project name The effect of temperature and PH on settlability ' + graduate);
-  Degree=Degree+1;
-} else if (project === 'no' || project === 'n') {
-  console.log('no thats not correct');
-  alert('no thats not correct' + graduate);
-}
+  var graduate = prompt('Is the graduation project name The effect of temperature and PH on settlability ?').toLocaleLowerCase();
+  if (graduate === 'yes' || graduate === 'y') {
+    console.log('yes thats correct my graduation project name The effect of temperature and PH on settlability');
+    alert( 'yes thats correct graduation project name The effect of temperature and PH on settlability ' + graduate);
+    Degree=Degree+1;
+  } else if (project === 'no' || project === 'n') {
+    console.log('no thats not correct');
+    alert('no thats not correct' + graduate);
+  }
 
 }
 myQuestion5();
@@ -97,60 +97,60 @@ document.write('welcome to my page  ' + yourname);
 function myQuestion6(){
 
 
-var userNumber = parseInt(prompt('what is my best number? choose from 1 to 7'));
+  var userNumber = parseInt(prompt('what is my best number? choose from 1 to 7'));
 
-for (var i = 0; i <= 3; i++) {
-  if (userNumber === 3) {
-    alert('correct');
-    Degree=Degree+1;
-    break;
-  } else if (userNumber === 5 || userNumber === 6 || userNumber === 7) {
-    console.log('too low');
-    alert(' (too low) no you are not close to the answer ' + userNumber);
-    userNumber = parseInt(prompt('what is my best number? choose from 1 to 7'));
-
-  } else if (userNumber === 1 || userNumber === 2 || userNumber === 4) {
-    console.log('too high');
-    alert('(too high) you are  close to the answer' + userNumber);
-    userNumber = parseInt(prompt('what is my best number? choose from 1 to 7'));
-
-  }
-}
-alert('Correct answer=3');
-
-}
- myQuestion6();
-
- function myQuestion7(){
-
-
-var myColor = ['white', 'blue', 'red', 'black', 'green', 'pink', 'silver'];
-var userColor = prompt('what is my favorite color?');
-var count = 0;
-console.log(userColor);
-for (var i = 0; i < 6; i++) {
-  for (var j = 0; j <= myColor.length - 1; j++) {
-    if (userColor === myColor[j]) {
+  for (var i = 0; i <= 3; i++) {
+    if (userNumber === 3) {
       alert('correct');
-      Degree = Degree+1;
+      Degree=Degree+1;
       break;
+    } else if (userNumber === 5 || userNumber === 6 || userNumber === 7) {
+      console.log('too low');
+      alert(' (too low) no you are not close to the answer ' + userNumber);
+      userNumber = parseInt(prompt('what is my best number? choose from 1 to 7'));
+
+    } else if (userNumber === 1 || userNumber === 2 || userNumber === 4) {
+      console.log('too high');
+      alert('(too high) you are  close to the answer' + userNumber);
+      userNumber = parseInt(prompt('what is my best number? choose from 1 to 7'));
+
+    }
+  }
+  alert('Correct answer=3');
+
+}
+myQuestion6();
+
+function myQuestion7(){
+
+
+  var myColor = ['white', 'blue', 'red', 'black', 'green', 'pink', 'silver'];
+  var userColor = prompt('what is my favorite color?');
+  var count = 0;
+  console.log(userColor);
+  for (var i = 0; i < 6; i++) {
+    for (var j = 0; j <= myColor.length - 1; j++) {
+      if (userColor === myColor[j]) {
+        alert('correct');
+        Degree = Degree+1;
+        break;
+      }
+      else {
+        count = count + 1;
+      }
+    }
+
+    if (count === 7) {
+      userColor = prompt('try again');
+      count = 0;
     }
     else {
-      count = count + 1;
+      break;
     }
   }
 
-  if (count === 7) {
-    userColor = prompt('try again');
-    count = 0;
-  }
-  else {
-    break;
-  }
+
+  alert(myColor);
+  alert('your Degree' + Degree);
 }
- }
 myQuestion7();
-
-
-alert(myColor);
-alert('your Degree' + Degree);
